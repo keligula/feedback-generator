ea1eb9236ba6be552041a5056baf47880cc9bc38
Kelly Smith <17773027+keligula@users.noreply.github.com>
2018-05-18 17:22:03 -0700

    Added Redux, React-Router

--- services/passport.js
+++ services/passport.js
@@ -23,16 +23,15 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then(existingUser => {
-                if (existingUser) {
-                    // we already have a record with the given profile id
-                    done(null, existingUser);
-                } else {
-                    // no record, so we need to create a new one
-                    new User({ googleId: profile.id }).save().then(user => done(null, user));
-                }
-            });
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id });
+
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);
         }
     )
 );
