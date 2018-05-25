ed7ca08f32c8e8cdf7a3347d9b874d7e399ba8fa
Kelly Smith <17773027+keligula@users.noreply.github.com>
2018-05-24 22:41:59 -0700

    added redirects dependent on login state

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Feedback Generator</h1>Collect feedback from your users.
+        </div>
+    );
+};
+
+export default Landing;
